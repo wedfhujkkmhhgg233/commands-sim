0e892214e22b0f2291b0979ba7663f61682772f3
wedfhujkkmhhgg233 <154974042+wedfhujkkmhhgg233@users.noreply.github.com>
2024-06-30 14:59:11 +0800

    Create teach(mirai).js

---
+++ teach(mirai).js
@@ -0,0 +1,35 @@
+const axios = require("axios");
+
+module.exports.config = {
+    name: "teach",
+    version: "1.0.0",
+    hasPermission: 0,
+    credits: "https://sim-api-ctqz.onrender.com/",
+    description: "Teach Simsimi",
+    usages: "Teach",
+    commandCategory: "...",
+    cooldowns: 0
+};
+
+module.exports.run = async ({ api, event, args }) => {
+    try {
+        const text = args.join(" ");
+        const text1 = text.substr(0, text.indexOf(' - '));
+        const text2 = text.split(" - ").pop();
+
+        if (!text1 || !text2) {
+            return api.sendMessage(`Usage: ${global.config.PREFIX}teach hi - hello`, event.threadID, event.messageID);
+        }
+
+        const response = await axios.get(`https://sim-api-ctqz.onrender.com/teach?ask=${encodeURIComponent(text1)}&ans=${encodeURIComponent(text2)}`);
+
+        if (response.data.message.includes("already learned")) {
+            return api.sendMessage(`Sim API has already learned this word.`, event.threadID, event.messageID);
+        }
+
+        api.sendMessage(`Your ask: ${text1}\nSim respond: ${text2}`, event.threadID, event.messageID);
+    } catch (error) {
+        console.error("An error occurred:", error);
+        api.sendMessage("Oops! Something went wrong.", event.threadID, event.messageID);
+    }
+};
